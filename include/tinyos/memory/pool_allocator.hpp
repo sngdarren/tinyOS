88eb1633c891b20e4c88f4a69db0b4bab19b580d
sngdarren <sngdarren99@gmail.com>
2026-08-12 16:57:07 +0800

    improvement: enforce block alignment in pool allocator

--- include/tinyos/memory/pool_allocator.hpp
+++ include/tinyos/memory/pool_allocator.hpp
@@ -8,15 +8,27 @@ namespace tinyos {
 class PoolAllocator final : public Allocator {
 
 public:
-    PoolAllocator(void* buffer, std::size_t capacity, std::size_t block_size)
+    PoolAllocator(void* buffer, std::size_t capacity, std::size_t block_size, 
+        std::size_t block_alignment)
         : base_(static_cast<std::byte*>(buffer)), capacity_(capacity), 
-        block_size_(block_size), free_head_(nullptr) {
+        block_alignment_(block_alignment), free_head_(nullptr) {
             
             // block size needs to be > size of FreeNode, because we cannot allocate a fraction of a FreeNode
             if (block_size < sizeof(FreeNode)) {
                 throw std::invalid_argument("The block size cannot be smaller than the size of a FreeNode!");
             }
 
+            else if (!is_power_of_two(block_alignment)) {
+                throw std::invalid_argument("Block alignment needs to be a power of 2");
+            }
+            
+            auto raw = reinterpret_cast<std::uintptr_t>(buffer);
+            auto aligned = align_up(raw, block_alignment);
+            base_ = reinterpret_cast<std::byte*>(aligned);
+
+            block_size_ = align_up(block_size, block_alignment);
+
+            capacity_ = ((static_cast<std::byte*>(buffer) + capacity) - base_);
             block_count_ = capacity_ / block_size_;
             stats_.bytes_reserved = block_count_ * block_size_;
             reset();
@@ -40,8 +52,9 @@ private:
     std::size_t capacity_;
     std::size_t block_size_;
     std::size_t block_count_;
+    std::size_t blocks_free_;
+    std::size_t block_alignment_;
     FreeNode* free_head_;
-
 };
 
 }
